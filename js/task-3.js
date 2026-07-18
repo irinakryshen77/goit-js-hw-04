f84f6178981fd947215aa01531476a5258737d7b
Irina Kryshen <irinakryshen77@gmail.com>
2026-07-18 23:32:41 +0300

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,23 @@
-function filterArray(numbers, value) {
-  const newArray = [];
-
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      newArray.push(numbers[i]);
-    }
+const profile = {
+    username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName
+      },
+  updatePlayTime(hours) {
+this.playTime += hours 
+// this.playTime = this.playTime + hours це означаэ що в звертаємость до властивості playTime об'єкта profile і збільшуємо її на величину параметра hours 
+  },
+  getInfo() {
+   return `${this.username} has ${this.playTime} active hours!`
   }
-
-  return newArray;
-}
+};
 
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
